bc66998df60f8a0b120f867814b4137346303f96
NataliyHusak <50527604+NataliyHusak@users.noreply.github.com>
2020-02-05 19:44:08 +0200

    Updates

---
+++ precache-manifest.2ee1f5bf932a1b471950ca127bf60bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e197b80854f438a72423e34d306b2979",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "d30c7b76af37350d8017",
+    "url": "/goit-react-hw-01-components/static/css/main.470b1025.chunk.css"
+  },
+  {
+    "revision": "f483be94e32cdf2914e4",
+    "url": "/goit-react-hw-01-components/static/js/2.3d422cf2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.3d422cf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d30c7b76af37350d8017",
+    "url": "/goit-react-hw-01-components/static/js/main.c718779f.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
